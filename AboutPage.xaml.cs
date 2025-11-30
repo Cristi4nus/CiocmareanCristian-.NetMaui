1fe9cafc1f37a3e6bf1180009f2265367f9e3ed0
Cristi4nus <cristianmircea19@gmail.com>
2025-11-30 18:41:48 +0200

    adaugat laboratorul 8

--- AboutPage.xaml.cs
+++ AboutPage.xaml.cs
@@ -2,18 +2,19 @@ namespace CiocmareanCristianLab7;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-		InitializeComponent();
-		Page = new ContentPage()
-		{
-			Content = new VerticalStackLayout
-			{
-				Children = {
-					new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
-					}
-				}
-			}
-		};
-	}
+    public AboutPage()
+    {
+        InitializeComponent();
+
+        this.Content = new VerticalStackLayout
+        {
+            Children = {
+                new Label {
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    Text = "Welcome to .NET MAUI!"
+                }
+            }
+        };
+    }
 }
